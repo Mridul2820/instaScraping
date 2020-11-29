1b41a64a36bb18d10d991d71bf2c8bc48f7bf00c
Mridul2820 <immridulpanda2820@gmail.com>
2020-11-29 21:21:49 +0530

    image , video, reels, igtv videos scrapping

---
+++ scrapers.js
@@ -0,0 +1,82 @@
+const puppeteer = require('puppeteer');
+
+
+// Image 
+async function scrapeImage(url) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    // Image
+    const [el] = await page.$x('//*[@id="react-root"]/section/main/div/div[1]/article/div[2]/div/div/div[1]/img');
+    const imgSrc = await el.getProperty('src');
+    const imageURL = await imgSrc.jsonValue();
+
+
+    console.log({imageURL});
+
+    browser.close();
+}
+
+scrapeImage('https://www.instagram.com/p/CIIjvBiAtf2/');
+scrapeImage('https://www.instagram.com/p/CILKyeKrCSz/');
+
+
+// Video
+async function scrapeVideo(url) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    // Video
+    const [el] = await page.$x('//*[@id="react-root"]/section/main/div/div[1]/article/div[2]/div/div/div[1]/div/div/video');
+    const VdoSrc = await el.getProperty('src');
+    const videoURL = await VdoSrc.jsonValue();
+
+    console.log({videoURL});
+
+    browser.close();
+}
+
+scrapeVideo('https://www.instagram.com/p/CIIm3JOh3IY/');
+scrapeVideo('https://www.instagram.com/reel/CILCIYtn2ZR/');
+
+
+// IGVideo
+async function scrapeIGVideo(url) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    // IGVideo
+    const [el] = await page.$x('//*[@id="react-root"]/section/main/div/div[1]/article/div[2]/div/div/div/div/div/video');
+    const VdoIGSrc = await el.getProperty('src');
+    const videoIGURL = await VdoIGSrc.jsonValue();
+
+    
+    console.log({videoIGURL});
+
+    browser.close();
+}
+
+scrapeIGVideo('https://www.instagram.com/tv/CHw7NYqKRgm/');
+
+
+// Caption
+async function scrapeCaption(url) {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(url);
+
+    // Caption
+    const [el] = await page.$x('/html/body/div[1]/section/main/div/div[1]/article/div[3]/div[1]/ul/div/li/div/div/div[2]/span');
+    const caption = await el.getProperty('textContent');
+    const captionTxt = await caption.jsonValue();
+
+
+    console.log({captionTxt});
+
+    browser.close();
+}
+
+scrapeCaption('https://www.instagram.com/p/CIIm3JOh3IY/');
